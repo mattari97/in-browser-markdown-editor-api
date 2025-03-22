9b3bc119690df692a24ae0733b68773f9f55b9b4
Mattari <ac.dev@hotmail.com>
2025-03-22 16:24:29 +0100

    Create Post model & seeders

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::inRandomOrder()->first()->id,
+            'title' => $this->faker->sentence(),
+            'body' => $this->createMarkdownBody(),
+        ];
+    }
+
+    private function createMarkdownBody()
+    {
+        return implode('\n\n', [
+            "# {$this->faker->sentence()}",
+            $this->faker->paragraph(),
+            '- ' . implode('\n-', $this->faker->sentences(3)),
+            "## {$this->faker->sentence()}",
+            $this->faker->paragraph(),
+            "```php\n\$value = \"Example Code\";\necho \$value;\n```",
+            "## {$this->faker->sentence()}",
+            $this->faker->paragraph(),
+            $this->faker->paragraph(),
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,10 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'email' => 'test@example.com',
-        ]);
+        User::factory(5)->create();
+        Post::factory(50)->create();
     }
 }
